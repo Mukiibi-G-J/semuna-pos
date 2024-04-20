17e43220ffd55c0520b51d98202e68d9e98d9790
Mukiibi-G-J <mukiibijosephgilber865@gmail.com>
2024-04-20 11:09:04 +0300

    changed api representation

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,15 @@ from product.models import Products
 
 
 class ProductsSerializers(serializers.ModelSerializer):
-    
+    price = serializers.FloatField(source='cost')
+    cost_price = serializers.FloatField(source='unit_price')
     class Meta:
         model = Products
         fields = '__all__'
+    
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['price'] = data.pop('cost')
+        data['cost_price'] = data.pop('unit_price')
+        return data
